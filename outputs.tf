c29aa9db38ad5cc57dfbd82e46f239d355bfd347
VipinSitomar <88337259+VipinSitomar@users.noreply.github.com>
2024-12-11 18:44:25 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "cluster_name" {
-  value       = google_container_cluster.primary.name
+  value       = google_container_cluster.main-cluster.name
   description = "Name of the GKE cluster"
 }
